b8efee2c657e731b4d4cc0b2217f29ecf2dfbb87
Erik <erikpastorrios1994@gmail.com>
2020-12-23 16:24:15 +0100

    Delete some data

--- src/view/js/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", (e) => {
-  e.preventDefault();
-  const chat = document.querySelector("#chat");
-  console.log(chat);
-  const messages = chat.import.querySelector("#messages");
-  document
-    .querySelector(".main-section")
-    .appendChild(document.importNode(messages, true));
-});
